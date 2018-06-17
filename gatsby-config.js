345b69316801d5aebe66c7dfc40cc2367f7fa58d
matt.wojas@gmail.com <matt.wojas@gmail.com>
2018-06-17 00:19:12 -0400

    add PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,19 @@ module.exports = {
           pathToConfigModule: `src/utils/typography.js`,
         },
       },
-      `gatsby-plugin-sass`
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: "GatsbyJS",
+          short_name: "GatsbyJS",
+          start_url: "/",
+          background_color: "#f7f0eb",
+          theme_color: "#a2466c",
+          display: "minimal-ui",
+          icon: "src/images/icon.png", // This path is relative to the root of the site.
+        },
+      },
+      `gatsby-plugin-sass`,
+      `gatsby-plugin-offline`
     ],
   };
